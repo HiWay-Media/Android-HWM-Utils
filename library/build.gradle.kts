2e112909be2202780ca20a0dd77c4010ae069a1e
allan <allan.nava@ies-italia.it>
2023-07-25 12:41:38 +0200

    added build gradle .kts

---
+++ library/build.gradle.kts
@@ -0,0 +1,51 @@
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+  alias(libs.plugins.plugin.android.library)
+  alias(libs.plugins.plugin.kotlin.android)
+  //id("mvn-publish")
+}
+
+val libVersionName = project.findProperty("VERSION_NAME") as String
+
+android {
+    namespace = "media.hiway.utils"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+        buildConfigField("String", "FCM_CLIENT_VERSION", "\"$libVersionName\"")
+    }
+    buildFeatures {
+      buildConfig = true
+    }
+    buildTypes {
+      getByName("release") {
+        isMinifyEnabled = false
+        proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+      }
+    }
+    compileOptions {
+      sourceCompatibility = JavaVersion.VERSION_1_8
+      targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+      jvmTarget = "1.8"
+      freeCompilerArgs = freeCompilerArgs + "-Xexplicit-api=strict"
+    }
+    publishing {
+      singleVariant("release") {
+        withSourcesJar()
+      }
+    }
+}
+
+dependencies {
+
+  implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+
+  implementation(libs.kotlin.stdLib)
+}
+
+/*tasks.dokkaHtml.configure {
+  outputDirectory.set(rootDir.resolve("docs"))
+}*/
